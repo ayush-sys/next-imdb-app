97f7a952536b2d921732a4685d3e83421b44ea7d
Ayush Pattanayak <ayushmp7@gmail.com>
2022-03-01 12:13:56 +0530

    Configured Firebase

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,10 +1,15 @@
 import Head from "next/head";
 import Image from "next/image";
 import { FaGoogle } from "react-icons/fa";
-import firebase from "../libs/firebase";
+import { FiLogOut } from "react-icons/fi";
+// import firebase from "../libs/firebase";
+import { signIn, signOut, useSession } from "next-auth/react";
+
 
 
 export default function login() {
+    const { data:session } = useSession();
+
   return (
     <div>
         <Head>
@@ -12,6 +17,17 @@ export default function login() {
             <link rel="icon" href="/hulu.png" />
         </Head>
 
+        {session ? (
+            <main className="flex flex-col items-center justify-center">
+                <h1 className="text-2xl text-white font-semibold tracking-wider">
+                    Congratulations !! You are succesfully logged in.
+                </h1>
+                <button className="login-btn" onClick={() => signOut()}>
+                    <FiLogOut className="h-4 w-4 mr-2"/>
+                    Logout
+                </button>
+            </main>
+        ) : (
         <main className="flex flex-col items-center justify-center">
             <Image 
                 src="https://links.papareact.com/ua6"
@@ -20,11 +36,22 @@ export default function login() {
                 width={400}
             />
 
-            <button className="login-btn">
+            <button className="login-btn" onClick={() => signIn()}>
                 <FaGoogle className="h-4 w-4 mr-2"/>
                 Login with Google
             </button>
         </main>
+        )}
+
+
+        {session && (
+            <div className="flex flex-row text-xl font-semibold text-white">
+                <p>Signed in as {session.user.email}</p>
+                <p>Name: {session.user.name}</p>
+            </div>
+        ) }
+
+        
     </div>
   )
 }
